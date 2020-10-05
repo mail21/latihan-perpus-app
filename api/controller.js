7f0092834fb6a6e9eebed3496788316bfd37114e
mail21 <ismailidn1@gmail.com>
2020-10-05 19:43:19 +0700

    menambah fitur delete buku

--- api/controller.js
+++ api/controller.js
@@ -1,35 +1,57 @@
-const db = require('./model');
+const table = require('./model');
 
 module.exports = {
   get: {
-    getBuku: (req, res) => {
-      db.query('SELECT * FROM buku', (err, result) => {
-        if (err) console.log(err);
-        res.json(result);
-      });
+    getBuku: async (req, res) => {
+      const query = table.select('*').from('buku');
+      const result = await query.then((data) => data);
+      res.json(result);
     },
-    getAnggota: (req, res) => {
-      db.query('SELECT * FROM anggota', (err, result) => {
-        if (err) console.log(err);
-        res.json(result);
-      });
+    getAnggota: async (req, res) => {
+      const query = table.select('*').from('anggota');
+      const result = await query.then((data) => data);
+      res.json(result);
     },
-    getPetugas: (req, res) => {
-      db.query('SELECT * FROM petugas', (err, result) => {
-        if (err) console.log(err);
-        res.json(result);
-      });
+    getPetugas: async (req, res) => {
+      const query = table.select('*').from('petugas');
+      const result = await query.then((data) => data);
+      res.json(result);
     },
-    getLokasi: (req, res) => {
-      db.query('SELECT * FROM lokasi', (err, result) => {
-        if (err) console.log(err);
-        res.json(result);
-      });
+    getLokasi: async (req, res) => {
+      const query = table.select('*').from('lokasi');
+      const result = await query.then((data) => data);
+      res.json(result);
+    },
+  },
+  delete: {
+    deleteBuku: async (req, res) => {
+      const idBuku = req.params.id;
+      const dataBuku = await table('buku')
+        .select('kuantitas', 'id_lokasi')
+        .where({ id: idBuku })
+        .then((res) => res);
+      const kuantitasAwal = await table('lokasi')
+        .select('kuantitas')
+        .where({ id: dataBuku[0].id_lokasi })
+        .then((res) => res[0].kuantitas);
+      await table('buku').where({ id: idBuku }).del();
+      res.send(`buku dengan id ${idBuku} berhasil dihapus`);
+      await table('lokasi')
+        .where({ id: dataBuku[0].id_lokasi })
+        .update({ kuantitas: kuantitasAwal - dataBuku[0].kuantitas });
     },
   },
   post: {
-    addBuku: (req, res) => {
-      // db.query("INSERT INTO buku VALUES(?,?,?,?,?)",[])
+    addBuku: async (req, res) => {
+      const { id, id_lokasi, judul, pengarang, kuantitas } = req.body;
+      let kuantitasAwal = table.select('kuantitas').from('lokasi').where('id', id_lokasi);
+      kuantitasAwal = await kuantitasAwal.then((data) => data[0].kuantitas);
+      await table('buku').insert(req.body);
+      await table('lokasi')
+        .where({ id: id_lokasi })
+        .update({ kuantitas: kuantitasAwal + kuantitas })
+        .then((res) => console.log(res));
+      res.send('sukses');
     },
   },
 };
