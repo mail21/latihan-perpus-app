b96ea87ae99cba339e39ca2fc756e65104f6305e
mail21 <ismailidn1@gmail.com>
2020-10-05 19:43:49 +0700

    Ganti mysql menjadi knex

--- api/model.js
+++ api/model.js
@@ -1,13 +1,24 @@
-const mysql = require('mysql');
-const koneksi = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'perpustakaan',
-});
+const knex = require('knex');
+// const mysql = require('mysql');
+// const koneksi = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   password: '',
+//   database: 'perpustakaan',
+// });
 
-koneksi.connect(() => {
-  console.log('telah berhasil konek db');
+// koneksi.connect(() => {
+//   console.log('telah berhasil konek db');
+// });
+
+const koneksi = knex({
+  client: 'mysql',
+  connection: {
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'perpustakaan',
+  },
 });
 
 module.exports = koneksi;
