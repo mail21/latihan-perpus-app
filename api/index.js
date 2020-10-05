5e78a525bc45a142c27dfdfd5717b14324a7cd0a
mail21 <ismailidn1@gmail.com>
2020-10-05 19:43:59 +0700

    Add Cors

--- api/index.js
+++ api/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const cors = require('cors');
 
 app.use(bodyParser.json());
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 require('./routes')(app);
