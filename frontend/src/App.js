ef0d75d9cdd1949f7a9d34ad56bfabeae8200981
mail21 <ismailidn1@gmail.com>
2020-10-06 13:10:32 +0700

    IMplementasi komponene komponene yang tadi dibuat

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import CardBook from './component/CardBook/CardBook';
+import Modal from './component/Modal/Modal';
+import axios from 'axios';
 
 function App() {
+  const [allBook, setAllBook] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  useEffect(() => {
+    axios.get('http://localhost:3000/getBuku').then((res) => setAllBook(res.data));
+  });
+
+  const deleteBook = (id) => {
+    let r = window.confirm('Are you Sure??');
+    if (r == true) {
+      axios.delete(`http://localhost:3000/deleteBuku/${id}`);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Perpustakaan</h1>
+      <button onClick={() => setIsModalOpen((prev) => !prev)}>Add New Buku +</button>
+      <br />
+      {isModalOpen ? <Modal isModalOpen={isModalOpen} set={setIsModalOpen} /> : <></>}
+      {allBook.map((el) => (
+        <CardBook key={el.id} {...el} deleteBook={deleteBook} />
+      ))}
     </div>
   );
 }
