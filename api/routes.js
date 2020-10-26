f8c5783569b57a67486cfdccd9efd2a538121b3f
mail21 <ismailidn1@gmail.com>
2020-10-26 17:35:59 +0700

    Add Route

--- api/routes.js
+++ api/routes.js
@@ -1,5 +1,5 @@
 module.exports = (app) => {
-  // http://localhost:3000/
+  // http://localhost:3001/
   const controller = require('./controller');
 
   app.get('/', (req, res) => {
@@ -8,13 +8,17 @@ module.exports = (app) => {
         - /getAnggota <br>
         - /getPetugas <br>
         - /getLokasi <br>
+        - /getTransaksi <br>
     `);
   });
 
   app.get('/getBuku', controller.get.getBuku);
+  app.get('/getTransaksi', controller.get.getTransaksi);
   app.get('/getAnggota', controller.get.getAnggota);
   app.get('/getPetugas', controller.get.getPetugas);
   app.get('/getLokasi', controller.get.getLokasi);
   app.post('/addBuku', controller.post.addBuku);
+  app.post('/addTransaksiPeminjaman', controller.post.addTransaksiPeminjaman);
+  app.post('/addTransaksiPengembalian', controller.post.addTransaksiPengembalian);
   app.delete('/deleteBuku/:id', controller.delete.deleteBuku);
 };
